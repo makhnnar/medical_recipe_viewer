604e2b3e75c9a90fbaf1ec3c18a9dd6a546340f4
Pedro Gomez <makhnnar@gmail.com>
2022-06-21 22:32:58 -0400

    added
    :heavy_plus_sign: added qr reader

---
+++ lib/qr_reader/qr_reader.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:qrscan/qrscan.dart' as scanner;
+
+class BarcodeScannerWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _BarcodeScannerWidget();
+  }
+}
+
+class _BarcodeScannerWidget extends State<BarcodeScannerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+  
+
+}
--- lib/recipe_list/recipe_list_view.dart
+++ lib/recipe_list/recipe_list_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:medical_recipe_viewer/data/recipe.dart';
+import 'package:medical_recipe_viewer/qr_reader/qr_reader.dart';
 import 'package:medical_recipe_viewer/recipe_creation/recipe_creation_view.dart';
 import 'package:medical_recipe_viewer/recipe_list/recipe_item_view.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'package:qrscan/qrscan.dart' as scanner;
 
 class RecipeListView extends StatelessWidget {
 
@@ -34,13 +38,25 @@ class RecipeListView extends StatelessWidget {
                 child: const Icon(Icons.add),
               ),
               FloatingActionButton(
-                onPressed: () {},
+                onPressed: () {
+                  scan();
+                },
                 child: Icon(Icons.search),
               )
             ],
           )
       )
     );
+  }
+
+  Future scan() async {
+    await Permission.camera.request();
+    String? barcode = await scanner.scan();
+    if (barcode == null) {
+      print('nothing return.');
+    } else {
+      print(barcode);
+    }
   }
 
   List<Widget> getList(List<Recipe> recipes) {
